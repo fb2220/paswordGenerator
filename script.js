3ee87ca92e5f88eab2d0adb83e74f4bb24d0bbc9
Fred Bernshtein <f0547494975@gmail.com>
2025-06-30 00:03:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+class PasswordGeerator {
+  constructor(len, upperCase, numbers, symbols) {
+    this.upperCase = upperCase;
+    this.numbers = numbers;
+    this.symbols = symbols;
+    this.len = parseInt(len);
+  }
+
+  genPass() {
+
+    let charset = "abcdefghijklmnopqrstuvwxyz";
+
+    if (this.upperCase === true) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (this.numbers) charset += "0123456789";
+    if (this.symbols) charset += "!@#$%^&*()_+-=[]{}|;:',.<>/?";
+
+    const s = chance.string({
+      length: this.len,
+      pool: charset,
+    });
+
+    return s
+  }
+
+
+
+}
+
+//variables
+
+// range
+
+const range = document.querySelector("#customRange1");
+
+// chk 1
+// chk 2
+// chk 3
+
+const checkBoxes = document.querySelectorAll('[type="checkbox"]');
+const checkBoxValues = [...checkBoxes].map((cb) => cb.checked);
+
+//input field
+
+const output = document.querySelector('input[type="text"]');
+
+// button
+const genButton = document.querySelector("#gen-button");
+
+// constrator
+
+
+
+//event listner
+genButton.addEventListener("click", () => {
+  const [upperCaseCB, numbersCB, symbolsCB] = checkBoxes;
+
+  const pwgt = new PasswordGeerator(
+    range.value,
+    upperCaseCB.checked,
+    numbersCB.checked,
+    symbolsCB.checked
+  );
+
+  output.value = pwgt.genPass();
+});
